1e375bb7ab063fb8e357b517631b948d129b80e0
TusksAndTasks <thedestroyerofworlds@mail.ru>
2021-10-25 04:51:33 +0300

    feat: add quotes and audioplayer

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -72,6 +72,7 @@ showGreeting();
 //background
 
 let randomNumber = 0;
+let backgroundNumber = 0;
 const body = document.querySelector('body');
 const leftArrow = document.querySelector('.slide-prev');
 const rightArrow = document.querySelector('.slide-next');
@@ -158,5 +159,284 @@ getWeatherReport();
 
 userCity.addEventListener('change', getWeatherReport)
 
+//quotes
+const quote = document.querySelector('.quote');
+const quoteAuthor = document.querySelector('.author');
+const changeQuoteButton = document.querySelector('.change-quote');
 
 
+async function getQuote(){
+    const quoteUrl = `https://type.fit/api/quotes`;
+    const quoteResponse = await fetch(quoteUrl);
+    const quoteData = await quoteResponse.json();
+    let randomQuoteNumber = Math.floor(Math.random() * 1463);
+
+    quote.textContent = quoteData[randomQuoteNumber].text;
+    quoteAuthor.textContent = quoteData[randomQuoteNumber].author;
+}
+
+getQuote();
+
+changeQuoteButton.addEventListener('click', getQuote);
+
+//audio
+
+
+const playList = [
+    {      
+      title: 'Aqua Caelestis',
+      src: '../momentum/assets/sounds/Aqua Caelestis.mp3',
+      duration: '00:39'
+    },
+    {
+      title: 'Ennio Morricone',
+      src: '../momentum/assets/sounds/Ennio Morricone.mp3',
+      duration: '01:37'
+    },  
+    {      
+      title: 'River Flows In You',
+      src: '../momentum/assets/sounds/River Flows In You.mp3',
+      duration: '01:38'
+    },
+    {
+      title: 'Summer Wind',
+      src: '../momentum/assets/sounds/Summer Wind.mp3',
+      duration: '01:50' 
+    }
+  ]
+
+let audioNumber = 0;
+const audio = new Audio();
+let isPlay = false;
+const playButton = document.querySelector('.play');
+let currentTime = 0;
+const prevPlay = document.querySelector('.play-prev');
+const nextPlay = document.querySelector('.play-next');
+const audioPlaylist = document.querySelector('.play-list');
+
+function createPlaylist(){
+    for(let i = 0; i < playList.length; i++) {
+        const audioList = document.createElement('li');
+        audioList.classList.add('play-item');
+        audioList.textContent = playList[i].title;
+        audioPlaylist.append(audioList);
+      }
+}
+
+createPlaylist();
+
+const audioActiveItem = document.querySelectorAll('.play-item');
+
+function playAudio() {
+audio.src = playList[audioNumber].src;
+audio.currentTime = currentTime;
+audio.play();
+isPlay = true;
+toggleButton();
+audioActiveItem[audioNumber].classList.add('item-active');
+}
+
+function pauseAudio() {
+    currentTime = audio.currentTime;
+    audio.pause();
+    isPlay = false;
+    toggleButton();
+}
+
+
+function toggleButton(){
+    if (isPlay){
+        playButton.classList.add('pause');
+    } else {
+        playButton.classList.remove('pause');
+    }
+}
+
+function togglePlay(){
+ if (isPlay){
+     pauseAudio();
+ } else {
+     playAudio();
+ }
+}
+
+function nextAudio(){
+    audioActiveItem[audioNumber].classList.remove('item-active');
+    if(audioNumber === 3){
+        audioNumber = 0;
+    } else{
+        audioNumber = audioNumber + 1;
+    }
+    audio.currentTime = 0;
+    pauseAudio();
+    getAudioData();
+}
+
+function prevAudio(){
+    audioActiveItem[audioNumber].classList.remove('item-active');
+    if(audioNumber === 0){
+        audioNumber = 3;
+    } else{
+        audioNumber = audioNumber - 1;
+    }
+    audio.currentTime = 0;
+    pauseAudio();
+    getAudioData();
+}
+
+function checkForEnding() {
+    if(audio.currentTime === audio.duration){
+        nextAudio();
+        togglePlay();
+    }
+}
+
+
+
+audio.addEventListener('pause', checkForEnding);
+playButton.addEventListener('click', togglePlay);      
+prevPlay.addEventListener('click', prevAudio);
+nextPlay.addEventListener('click', nextAudio);
+
+//audio-pro
+
+const audioTitle = document.querySelector('.audio-title');
+const audioDuration = document.querySelector('.duration');
+const audioCurrentTime = document.querySelector('.current-time');
+const audioProgressBar = document.querySelector('.progress-big');
+let mousedown = false;
+const volumeButton = document.querySelector('.volume');
+const volumeBar = document.querySelector('.progress-small');
+const smallPlays = document.querySelectorAll('.small-play');
+
+function transformCurrentTime(){
+    let minutes = Math.floor(audio.currentTime / 60).toString().padStart(2, 0);
+    let seconds = Math.floor(audio.currentTime - (minutes * 60)).toString().padStart(2, 0);
+    audioCurrentTime.textContent = `${minutes}:${seconds}`;
+    setTimeout(transformCurrentTime, 300);
+}
+
+transformCurrentTime();
+
+function getAudioData(){
+    audioTitle.textContent = playList[audioNumber].title;
+    audioDuration.textContent = playList[audioNumber].duration;
+}
+
+getAudioData();
+
+function handleProgress() {
+    let percent = (audio.currentTime / audio.duration) * 100;
+    if (isNaN(percent)){
+       percent = 0;
+       audioProgressBar.style.background = `linear-gradient(to right, #2370a3 0%, #2370a3 ${percent}%, #C4C4C4 ${percent}%, #C4C4C4 100%)`;
+    }
+    else {audioProgressBar.style.background = `linear-gradient(to right, #2370a3 0%, #2370a3 ${percent}%, #C4C4C4 ${percent}%, #C4C4C4 100%)`;}
+ }
+  
+ function scrub (e) {
+    currentTime = (e.offsetX / audioProgressBar.offsetWidth) * audio.duration;
+    audio.currentTime = currentTime;
+ }
+
+
+ function handleRangeUpdate(){
+    audio.volume = volumeBar.value;
+    let volumeValue = volumeBar.value * 100;
+    this.style.background = `linear-gradient(to right, #2370a3 0%, #2370a3 ${volumeValue}%, #C4C4C4 ${volumeValue}%, #C4C4C4 100%)`
+  };
+  
+  function updateVolumeButton() {
+     if (audio.volume === 0){
+        volumeButton.style.backgroundImage = 'url(../momentum/assets/svg/mute.svg)';
+     }
+     else {
+        volumeButton.style.backgroundImage = 'url(../momentum/assets/svg/volume.svg)';
+     }
+  }
+
+  function toggleVolume(){
+    if (audio.volume === 0){
+        audio.volume = 0.5;
+        updateVolumeButton();
+    } else {
+        audio.volume = 0;
+        updateVolumeButton();
+    }
+  }
+
+
+smallPlays[0].onclick = function(){
+    if(isPlay && audioNumber === 0){
+        pauseAudio();
+    } 
+    else if (!isPlay && audioNumber === 0){
+        playAudio();
+    } else {
+    pauseAudio();
+    audioActiveItem[audioNumber].classList.remove('item-active');
+    audioNumber = 0;
+    currentTime = 0;
+    getAudioData();
+    playAudio();
+}
+};
+smallPlays[1].onclick = function(){
+    if(isPlay && audioNumber === 1){
+        pauseAudio();
+    }   
+    else if (!isPlay && audioNumber === 1){
+        playAudio();
+   } else {
+   pauseAudio();
+   audioActiveItem[audioNumber].classList.remove('item-active');
+   audioNumber = 1;
+   currentTime = 0;
+   getAudioData();
+   playAudio();
+}
+};
+smallPlays[2].onclick = function(){
+    if(isPlay && audioNumber === 2){
+        pauseAudio();
+    }   
+    else if (!isPlay && audioNumber === 2){
+        playAudio();
+   } else {
+   pauseAudio();
+   audioActiveItem[audioNumber].classList.remove('item-active');
+   audioNumber = 2;
+   currentTime = 0;
+   getAudioData();
+   playAudio();
+}
+};
+smallPlays[3].onclick = function(){
+    if(isPlay && audioNumber === 3){
+        pauseAudio();
+    }   
+    else if (!isPlay && audioNumber === 3){
+        playAudio();
+   } else {
+   pauseAudio();
+   audioActiveItem[audioNumber].classList.remove('item-active');
+   audioNumber = 3;
+   currentTime = 0;
+   getAudioData();
+   playAudio();
+}
+};
+
+ audio.addEventListener('timeupdate', handleProgress);
+ audioProgressBar.addEventListener('click', scrub);
+ audioProgressBar.addEventListener('mousedown', () => mousedown = true);
+ audioProgressBar.addEventListener('mouseup', () => mousedown = false);
+ audioProgressBar.addEventListener('mousemove', (e) => mousedown && scrub(e));
+ volumeBar.addEventListener('change', handleRangeUpdate);
+ volumeBar.addEventListener('mousemove', handleRangeUpdate);
+ volumeBar.addEventListener('touchmove', handleRangeUpdate);
+ volumeBar.addEventListener('change', updateVolumeButton);
+ volumeBar.addEventListener('mousemove', updateVolumeButton);
+ volumeBar.addEventListener('touchmove', updateVolumeButton);
+ volumeButton.addEventListener('click', toggleVolume);
+ 
